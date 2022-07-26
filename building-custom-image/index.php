13b3068806e736209f3f7497cea5a77c0195c1f5
DiegoMarty <contacto@diegomarty.com>
2022-07-26 12:42:01 +0200

    ✨ Added Twig Hello World

--- building-custom-image/index.php
+++ building-custom-image/index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo "hello world";
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new Twig_Loader_Array(array(
+    'index' => 'Hello {{ name }}!',
+));
+$twig = new Twig_Environment($loader);
+
+echo $twig->render('index', array('name' => 'Fabien'));
